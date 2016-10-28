363fe502126f6b06f1643ba2dce3ccd2e149b37e
Subhani Shaik <subhanis@codeaurora.org>
2016-10-27 23:08:20 -0700

    [AWARE]: Enable Discovery MAC addr randomization
    By default discovery MAC address randomization is enabled
    and default(maximum) interval value is 30 minutes(1800 seconds).
    Nan enable and config can specify the interval value in seconds.
    Value of zero disables discovery MAC address randomization.
    
    Change-Id: I5dc10db8535227575f96635604665b6f52f8d507

--- include/hardware_legacy/wifi_nan.h
+++ include/hardware_legacy/wifi_nan.h
@@ -783,8 +783,13 @@ typedef struct {
     u8 config_5g_channel;
     wifi_channel channel_5g_val;
 
-    /* Configure 2.4/5GHz DW */
-    NanConfigDW config_dw;
+    /*
+       By default discovery MAC address randomization is enabled
+       and default interval value is 30 minutes i.e. 1800 seconds.
+       The value 0 is used to disable MAC addr randomization.
+    */
+    u8 config_disc_mac_addr_randomization;
+    u16 disc_mac_addr_rand_interval_sec;
 } NanEnableRequest;
 
 /*
@@ -1163,8 +1168,13 @@ typedef struct {
     /* NAN Further availability Map */
     u8 config_fam;
     NanFurtherAvailabilityMap fam_val;
-    /* Configure 2.4/5GHz DW */
-    NanConfigDW config_dw;
+    /*
+       By default discovery MAC address randomization is enabled
+       and default interval value is 30 minutes i.e. 1800 seconds.
+       The value 0 is used to disable MAC addr randomization.
+    */
+    u8 config_disc_mac_addr_randomization;
+    u16 disc_mac_addr_rand_interval_sec;
 } NanConfigRequest;
 
 /*
